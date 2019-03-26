3ea991f7e843e43dee509d39a9b0d0137f9bec29
Jhonatan Medeiros <jhonatan.medeiros02@gmail.com>
2019-03-26 00:13:15 -0300

    Added JavaFlix Class and change .gitignore

---
+++ src/main/java/br/com/javaflix/JavaFlix.java
@@ -0,0 +1,11 @@
+package br.com.javaflix;
+
+/**
+ *
+ * @author Jhonatan Medeiros
+ */
+public class JavaFlix {
+    public static void main(String[] args) {
+        // System.out.println("Working !");
+    }
+}
