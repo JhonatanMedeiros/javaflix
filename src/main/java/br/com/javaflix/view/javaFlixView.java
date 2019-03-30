1fc9c48ee7f1ec9238f37b2b076d735f4f0f8597
madeofsky <blslucasfj1@hotmail.com>
2019-03-30 16:59:29 -0300

    Main Panel and GridLayout created

--- src/main/java/br/com/javaflix/view/javaFlixView.java
+++ src/main/java/br/com/javaflix/view/javaFlixView.java
@@ -5,10 +5,54 @@
  */
 package br.com.javaflix.view;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+    
 /**
  *
- * @author blslu
+ * @author Lucas Fernandes de Jesus
  */
-public class javaFlixView {
-    
+public class javaFlixView extends JFrame {
+
+       public static void main(final String[]args) {
+
+           JFrame.setDefaultLookAndFeelDecorated(true);
+           JFrame frame = new JFrame("Layout");
+           frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+           
+           // Criando painel principal
+           JPanel panelUm = new JPanel();
+           panelUm.setLayout(new BorderLayout());
+           
+           // GridLayout para os filmes
+           JPanel moviesPanel = new JPanel();
+           moviesPanel.setLayout(new GridLayout(5, 5, 25, 54));
+           
+           //Navbar
+           JTextField navbar = new JTextField("JavaFlix");
+           
+           // Cards clicáveis com os filmes
+           JButton filmeUm = new JButton("Filme 1");
+           JButton filmeDois = new JButton("Filme 2");
+           JButton filmeTres = new JButton("Filme 3");
+           JButton filmeQuatro = new JButton("Filme 4");
+           JButton filmeCinco = new JButton("Filme 5");
+           JButton filmeSeis = new JButton("Filme 6");
+           
+           moviesPanel.add(filmeUm);
+           moviesPanel.add(filmeDois);
+           moviesPanel.add(filmeTres);
+           moviesPanel.add(filmeQuatro);
+           moviesPanel.add(filmeCinco);
+           moviesPanel.add(filmeSeis);
+           
+           panelUm.add(navbar, BorderLayout.NORTH);
+           panelUm.add(moviesPanel, BorderLayout.CENTER);
+           panelUm.setVisible(true);
+           panelUm.setSize(550, 550);
+       }
 }
